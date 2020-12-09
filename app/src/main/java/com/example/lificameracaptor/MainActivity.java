56cf8d2c1a3779740e08502990a4782b9e43af39
molpy1996 <36535002+molpy1996@users.noreply.github.com>
2020-12-09 12:22:19 +0100

    added a textview and connected the button to the permission request function added a framelayout to display camera view

--- app/src/main/java/com/example/lificameracaptor/MainActivity.java
+++ app/src/main/java/com/example/lificameracaptor/MainActivity.java
@@ -1,14 +1,118 @@
 package com.example.lificameracaptor;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import net.oledcomm.sdk.LiFiCallback;
+import net.oledcomm.sdk.LiFiSdkManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int STORAGE_PERMISSION_CODE = 1;
+
+    private LiFiSdkManager liFiSdkManager;
+
+    TextView textView;
+    FrameLayout camera;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textview);
+
+        camera = findViewById(R.id.main_layout);
+
+        Button buttonRequest = findViewById(R.id.button);
+        buttonRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this, "You have already granted this permission!",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    requestStoragePermission();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        liFiSdkManager = new LiFiSdkManager(this, new LiFiCallback() {
+            @Override
+            public void onLiFiPositionUpdate(String lamp) {
+
+                /* Add your actions here.
+                 *  lamp will contain the tag (eg. 10101010) if decoding was successful.
+                 */
+
+                textView.setText(lamp);
+            }
+
+            @Override
+            public void onLiFiErrorUpdate(String errorMessage) {
+                /* Add your actions here.
+                 *  If there was an error, lamp could contain error related text like "No lamp detected" or "Weak signal", etc.
+                 */
+                textView.setText(errorMessage);
+            }
+        });
+
+        liFiSdkManager.start(R.id.main_layout, LiFiSdkManager.FRONT_CAMERA);
+    }
+
+    private void requestStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("This permission is needed because of this and that")
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this,
+                                    new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .create().show();
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == STORAGE_PERMISSION_CODE)  {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission GRANTED", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
